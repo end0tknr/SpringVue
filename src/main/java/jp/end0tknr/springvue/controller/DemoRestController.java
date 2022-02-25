3cb94f8f8b33478112a2e2c4d0f0bc7d9ae8dff1
end0tknr <end0tknr@gmail.com>
2022-02-26 05:09:55 +0900

    add demo and test

--- src/main/java/jp/end0tknr/springvue/controller/DemoRestController.java
+++ src/main/java/jp/end0tknr/springvue/controller/DemoRestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoRestController {
 
-    @RequestMapping("/hoge")
+    @RequestMapping("/")
     public String index() {
         return "Hello Spring Boot !!";
     }
