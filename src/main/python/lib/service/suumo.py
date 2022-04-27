2bb0742b6a147279c37b90803a70a92a7f894bd8
end0tknr <end0tknr@gmail.com>
2022-04-28 08:12:12 +0900

    modify suumo.py

--- src/main/python/lib/service/suumo.py
+++ src/main/python/lib/service/suumo.py
@@ -41,10 +41,67 @@ logger = appbase.AppBase.get_logger()
 
 class SuumoService(appbase.AppBase):
     
+    def __init__(self):
+        pass
+    
+
     def find_search_result_list_url(self):
+        logger.info("start")
         
         for base_url in base_urls:
-            browser = init_browser(self)
+            for pref_name in pref_names:
 
-    def init_browser():
-        pass
+                #他の都道府県のurl構成が異なる為、無視(skip)
+                if pref_name == "hokkaido" and \
+                     base_url == "https://suumo.jp/ms/shinchiku/":
+                    continue
+                
+                #「hokkaido_」のように「_」が付加されている為
+                if pref_name == "hokkaido" and \
+                   base_url in ("https://suumo.jp/ikkodate/",
+                                "https://suumo.jp/chukoikkodate/",
+                                "https://suumo.jp/ms/chuko/"):
+                    pref_name += "_"
+                
+                
+                print("%s %s" %(base_url, pref_name))
+
+
+    def find_search_result_list_url_sub(self, base_url, pref_name):
+        logger.info("%s %s" % (base_url, pref_name))
+
+        req_url = base_url + pref_name +"/city/"
+        browser.get( req_url )
+
+        # 検索ボタン click
+        submit_btns = \
+            browser.find_elements_by_css_selector(
+                ".ui-btn--search"
+            )
+
+        if len(submit_btns) == 0:
+            print("ERROR find_elements_by_css_selector() ",req_url, file=sys.stderr)
+            sys.exit()
+            return []
+
+        submit_btns[0].click()
+        time.sleep(2)
+
+        paginations = []
+        paginations.extend(
+            browser.find_elements_by_css_selector(
+                ".pagination.pagination_set-nav ol li")
+        )
+        paginations.extend(
+            browser.find_elements_by_css_selector(
+                ".sortbox_pagination ol li")
+        )
+
+        ret_urls = [browser.current_url]
+        if len(paginations) == 0:
+            return ret_urls
+
+        for pno in range( 1, int(paginations[-1].text) ):
+            ret_urls.append("%s&pn=%d" % (browser.current_url, pno+1) )
+
+        return ret_urls
