81490edf469df1077d52e691bd7fdf8a57f8ee65
end0tknr <end0tknr@gmail.com>
2022-04-28 09:36:02 +0900

    midify suumo.py

--- src/main/python/batch/save_suumo.py
+++ src/main/python/batch/save_suumo.py
@@ -9,7 +9,8 @@ from service.suumo import SuumoService
 def main():
     suumo_service = SuumoService()
     result_list_urls = suumo_service.find_search_result_list_url()
-    
+    print( result_list_urls )
+
 
 
 if __name__ == '__main__':
--- src/main/python/lib/service/suumo.py
+++ src/main/python/lib/service/suumo.py
@@ -1,6 +1,10 @@
 #!python
 # -*- coding: utf-8 -*-
 
+from selenium import webdriver # ex. pip install selenium==4.1.3
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+
 import appbase
 import json
 import re
@@ -8,13 +12,14 @@ import time
 import urllib.parse
 import urllib.request
 
-conf = {
-    "chrome_options" : [#"--headless",
-                        "--enable-logging=False",
-                        #以下、3行はSSLエラー対策らしい
-                        "--ignore-certificate-errors",
-                        "--disable-extensions",
-                        "--disable-print-preview"],
+browser_conf = {
+    "browser_options" : [
+        #"--headless",
+        "--enable-logging=False",
+        #以下、3行はSSLエラー対策らしい
+        "--ignore-certificate-errors",
+        "--disable-extensions",
+        "--disable-print-preview"],
     "implicitly_wait": 10 }
 
 pref_names = [
@@ -47,10 +52,10 @@ class SuumoService(appbase.AppBase):
 
     def find_search_result_list_url(self):
         logger.info("start")
-        
+
+        ret_urls = []
         for base_url in base_urls:
             for pref_name in pref_names:
-
                 #他の都道府県のurl構成が異なる為、無視(skip)
                 if pref_name == "hokkaido" and \
                      base_url == "https://suumo.jp/ms/shinchiku/":
@@ -63,39 +68,39 @@ class SuumoService(appbase.AppBase):
                                 "https://suumo.jp/ms/chuko/"):
                     pref_name += "_"
                 
-                
-                print("%s %s" %(base_url, pref_name))
-
+                tmp_urls = self.find_search_result_list_url_sub(base_url,
+                                                                pref_name)
+                ret_urls.extend(tmp_urls)
+        return ret_urls
+    
 
     def find_search_result_list_url_sub(self, base_url, pref_name):
         logger.info("%s %s" % (base_url, pref_name))
-
+        browser = self.init_browser()
+        
         req_url = base_url + pref_name +"/city/"
         browser.get( req_url )
-
+        
         # 検索ボタン click
+        css_selector = ".ui-btn--search"
         submit_btns = \
-            browser.find_elements_by_css_selector(
-                ".ui-btn--search"
-            )
+            browser.find_elements_by_css_selector(css_selector)
 
         if len(submit_btns) == 0:
-            print("ERROR find_elements_by_css_selector() ",req_url, file=sys.stderr)
-            sys.exit()
+            logger.error(req_url +" "+css_selector)
+            browser.close()
             return []
 
         submit_btns[0].click()
-        time.sleep(2)
+        time.sleep(3)
 
         paginations = []
         paginations.extend(
             browser.find_elements_by_css_selector(
-                ".pagination.pagination_set-nav ol li")
-        )
+                ".pagination.pagination_set-nav ol li") )
         paginations.extend(
             browser.find_elements_by_css_selector(
-                ".sortbox_pagination ol li")
-        )
+                ".sortbox_pagination ol li") )
 
         ret_urls = [browser.current_url]
         if len(paginations) == 0:
@@ -104,4 +109,20 @@ class SuumoService(appbase.AppBase):
         for pno in range( 1, int(paginations[-1].text) ):
             ret_urls.append("%s&pn=%d" % (browser.current_url, pno+1) )
 
+        browser.close()
         return ret_urls
+
+
+    def init_browser(self):
+        driver_path = self.get_conf()["common"]["browser_driver"]
+        browser_service = Service( executable_path=driver_path )
+
+        browser_opts = Options()
+        for tmp_opt in browser_conf["browser_options"]:
+            browser_opts.add_argument( tmp_opt )
+
+        browser = webdriver.Edge(service = browser_service,
+                                 options = browser_opts )
+        # 要素が見つかるまで、最大 ?秒 待つ
+        browser.implicitly_wait( browser_conf["implicitly_wait"] )
+        return browser
