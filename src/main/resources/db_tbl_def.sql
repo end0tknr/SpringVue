b97c4d29ee3543947249c582b50c1998d1410afc
end0tknr <end0tknr@gmail.com>
2022-05-04 15:26:36 +0900

    add comment

--- src/main/resources/db_tbl_def.sql
+++ src/main/resources/db_tbl_def.sql
@@ -12,13 +12,15 @@ city            varchar(8),
 lng             double precision,
 lat             double precision,
 primary key(code));
+COMMENT ON COLUMN city.code IS '市町村code';
 
-CREATE TABLE IF NOT EXISTS estat_jutakutochi (
-city              varchar(8),
-setai             int,
-setai_nushi_age   varchar(1024),
-setai_year_income varchar(1024),
-primary key(city) );
+-- 2015年の古いデータの為、対象外
+-- CREATE TABLE IF NOT EXISTS estat_jutakutochi (
+-- city              varchar(8),
+-- setai             int,
+-- setai_nushi_age   varchar(1024),
+-- setai_year_income varchar(1024),
+-- primary key(city) );
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_d001 (
 pref            varchar(4),
@@ -27,6 +29,13 @@ house             bigint,
 lived_house       bigint,
 nolived_house     bigint,
 primary key(pref,city) );
+
+COMMENT ON TABLE estat_jutakutochi_d001 IS
+'https://www.e-stat.go.jp/stat-search/files
+  ?layout=datalist&toukei=00200522&tstat=000001127155&tclass1=000001133386
+市区町村-1
+  居住世帯の有無(8区分)別住宅数及び住宅以外で人が
+  居住する建物数―全国，都道府県，市区町村';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_d002 (
 pref            varchar(4),
@@ -38,6 +47,17 @@ apartment       bigint,
 owned_house     bigint,
 rented_house    bigint,
 primary key(pref,city) );
+COMMENT ON TABLE estat_jutakutochi_d002 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&tclass1=000001133386
+市区町村-2
+ 住宅の建て方(3区分)，階数(4区分)・構造(2区分)・所有の関係(2区分)・
+ 建築の時期(2区分)別住宅数及び世帯の種類(2区分)別世帯数―全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_d002.detached_house  IS '一戸建';
+COMMENT ON COLUMN estat_jutakutochi_d002.tenement_houses IS '長屋建';
+COMMENT ON COLUMN estat_jutakutochi_d002.apartment       IS '共同住宅';
+COMMENT ON COLUMN estat_jutakutochi_d002.owned_house     IS '持ち家';
+COMMENT ON COLUMN estat_jutakutochi_d002.rented_house    IS '借家';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e006 (
 pref            varchar(4),
@@ -47,6 +67,14 @@ total           bigint,
 owned_house     bigint,
 rented_house    bigint,
 primary key(pref,city,build_year) );
+COMMENT ON TABLE estat_jutakutochi_e006 IS
+'https://www.e-stat.go.jp/stat-search/files
+  ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+   &tclass1=000001129435&tclass2=000001129436
+住宅の種類，建て方，建築の時期，建物の構造，階数 6-3
+  住宅の所有の関係(5区分)，建築の時期(7区分)別住宅数－全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_e006.owned_house     IS '持ち家';
+COMMENT ON COLUMN estat_jutakutochi_e006.rented_house    IS '借家';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e030 (
 pref            varchar(4),
@@ -57,6 +85,16 @@ solar_water_heater      bigint,
 pv                      bigint,
 double_sash             bigint,
 primary key(pref,city,own_type) );
+COMMENT ON TABLE estat_jutakutochi_e030 IS
+'https://www.e-stat.go.jp/stat-search/files
+  ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+  &tclass1=000001129435&tclass2=000001129436
+住宅の設備 30-2
+  住宅の種類(2区分)，住宅の所有の関係(2区分)，建て方(4区分)，構造(2区分)，
+  省エネルギー設備等(7区分)別住宅数－全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_e030.solar_water_heater IS '太陽熱温水';
+COMMENT ON COLUMN estat_jutakutochi_e030.pv                 IS '太陽光発電';
+COMMENT ON COLUMN estat_jutakutochi_e030.double_sash        IS '二重以上のサッシ';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e033 (
 pref            varchar(4),
@@ -66,6 +104,15 @@ build_year      varchar(16),
 owned_house     bigint,
 rented_house    bigint,
 primary key(pref,city,damage,build_year) );
+COMMENT ON TABLE estat_jutakutochi_e033 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+住宅の設備 33-2
+ 住宅の所有の関係(6区分)，腐朽・破損の有無(2区分)，建築の時期(9区分)別住宅数
+ －全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_e033.owned_house     IS '持ち家';
+COMMENT ON COLUMN estat_jutakutochi_e033.rented_house    IS '借家';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e044 (
 pref              varchar(4),
@@ -74,6 +121,15 @@ own_type          varchar(4),
 year_income       varchar(32),
 setai             bigint,
 primary key(pref,city,own_type,year_income) );
+COMMENT ON TABLE estat_jutakutochi_e044 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+住宅の設備 44-4
+  世帯の年間収入階級(9区分)，世帯の種類(2区分)，
+  住宅の所有の関係(5区分)別普通世帯数，１世帯当たり人員，
+  １世帯当たり居住室数及び１世帯当たり居住室の畳数－全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_e044.year_income IS '年収';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e048 (
 pref              varchar(4),
@@ -87,6 +143,13 @@ owner_age_55_64 bigint,
 owner_age_65    bigint,
 owner_age_unknown bigint,
 primary key(pref,city,build_year) );
+COMMENT ON TABLE estat_jutakutochi_e048 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+家計を主に支える者と住居 48-2
+ 住宅の建築の時期(7区分)，建て方(2区分)，構造(2区分)別家計を主に
+ 支える者の年齢(6区分)別主世帯数及び平均年齢－全国，都道府県，市区町村';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e049 (
 pref            varchar(4),
@@ -104,6 +167,14 @@ rent_150000_199999      bigint,
 rent_200000             bigint,
 rent_unknown            bigint,
 primary key(pref,city,owner_age) );
+COMMENT ON TABLE estat_jutakutochi_e049 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+家計を主に支える者と住居 49-2
+  家計を主に支える者の年齢(6区分)別世帯の１か月当たり家賃(10区分)
+  別借家に居住する主世帯数及び１か月当たり家賃－全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_e049.rent_0 IS '家賃';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_e101 (
 pref            varchar(4),
@@ -116,6 +187,18 @@ rebuild         bigint,
 inheritance     bigint,
 other           bigint,
 primary key(pref,city,build_year) );
+COMMENT ON TABLE estat_jutakutochi_e101 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+持ち家の購入･新築･建て替え等の状況 101-3
+  住宅の建築の時期(7区分)，住宅の購入・新築・建て替え等(8区分)別持ち家数
+  －全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_e101.buy_new     IS '新築の住宅を購入';
+COMMENT ON COLUMN estat_jutakutochi_e101.buy_used    IS '中古住宅を購入';
+COMMENT ON COLUMN estat_jutakutochi_e101.build_new   IS '新築';
+COMMENT ON COLUMN estat_jutakutochi_e101.rebuild     IS '建て替え';
+COMMENT ON COLUMN estat_jutakutochi_e101.inheritance IS '相続・贈与';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_g157 (
 pref            varchar(4),
@@ -129,6 +212,25 @@ reform_pillar_basic     bigint,
 reform_insulation       bigint,
 reform_other            bigint,
 primary key(pref,city,build_year) );
+COMMENT ON TABLE estat_jutakutochi_g157 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+持ち家の増改築・改修工事,高齢者等のための設備工事,
+耐震改修工事,耐震診断の有無,リフォーム工事の状況 157-3
+  住宅の建築の時期(7区分)，2014年以降の住宅の増改築
+  ・改修工事等(8区分)別持ち家数－全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_g157.reform_plan  IS '増築・間取りの変更';
+COMMENT ON COLUMN estat_jutakutochi_g157.reform_kitchen_bath
+                                     IS '台所・トイレ・浴室・洗面所の改修工事';
+COMMENT ON COLUMN estat_jutakutochi_g157.reform_floor_inner_wall
+                                     IS '天井・壁・床等の内装の改修工事';
+COMMENT ON COLUMN estat_jutakutochi_g157.reform_roof_outer_wall
+                                     IS '屋根・外壁等の改修工事';
+COMMENT ON COLUMN estat_jutakutochi_g157.reform_pillar_basic
+                                     IS '壁・柱・基礎等の補強工事';
+COMMENT ON COLUMN estat_jutakutochi_g157.reform_insulation
+                                     IS '窓・壁等の断熱・結露防止工事';
 
 CREATE TABLE IF NOT EXISTS estat_jutakutochi_g158 (
 pref            varchar(4),
@@ -142,6 +244,25 @@ reform_pillar_basic     bigint,
 reform_insulation       bigint,
 reform_other            bigint,
 primary key(pref,city,year_income) );
+COMMENT ON TABLE estat_jutakutochi_g158 IS
+'https://www.e-stat.go.jp/stat-search/files
+ ?layout=datalist&toukei=00200522&tstat=000001127155&cycle=0
+ &tclass1=000001129435&tclass2=000001129436
+持ち家の増改築・改修工事,高齢者等のための設備工事,
+耐震改修工事,耐震診断の有無,リフォーム工事の状況 158-4
+世帯の年間収入階級(5区分)，2014年以降の住宅の増改築・改修工事等(8区分)
+ 別持ち家数－全国，都道府県，市区町村';
+COMMENT ON COLUMN estat_jutakutochi_g158.reform_plan  IS '増築・間取りの変更';
+COMMENT ON COLUMN estat_jutakutochi_g158.reform_kitchen_bath
+                                     IS '台所・トイレ・浴室・洗面所の改修工事';
+COMMENT ON COLUMN estat_jutakutochi_g158.reform_floor_inner_wall
+                                     IS '天井・壁・床等の内装の改修工事';
+COMMENT ON COLUMN estat_jutakutochi_g158.reform_roof_outer_wall
+                                     IS '屋根・外壁等の改修工事';
+COMMENT ON COLUMN estat_jutakutochi_g158.reform_pillar_basic
+                                     IS '壁・柱・基礎等の補強工事';
+COMMENT ON COLUMN estat_jutakutochi_g158.reform_insulation
+                                     IS '窓・壁等の断熱・結露防止工事';
 
 CREATE TABLE IF NOT EXISTS mlit_fudousantorihiki (
 id              serial,
