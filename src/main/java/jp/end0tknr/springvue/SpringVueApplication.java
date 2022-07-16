c2c47ca048a4d231491a518d673b2108e35000c5
end0tknr <end0tknr@gmail.com>
2022-07-16 15:50:01 +0900

    build war packaging

--- src/main/java/jp/end0tknr/springvue/SpringVueApplication.java
+++ src/main/java/jp/end0tknr/springvue/SpringVueApplication.java
@@ -2,9 +2,11 @@ package jp.end0tknr.springvue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringVueApplication {
+public class SpringVueApplication extends SpringBootServletInitializer {
+//public class SpringVueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringVueApplication.class, args);
